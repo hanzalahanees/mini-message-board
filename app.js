50a3d29ab28dee0b68ec7f094043df39e5a4efa1
hanzalahanees <44531147+hanzalahanees@users.noreply.github.com>
2024-01-15 11:12:58 +0500

    Update app.js

--- app.js
+++ app.js
@@ -19,6 +19,12 @@ const app = express();
 
 // Set up rate limiter: maximum of twenty requests per minute
 // const RateLimit = require("express-rate-limit");
+// Express-rate-limit is a middleware package that can be used to limit repeated requests to APIs
+// and endpoints. There are many reasons why excessive requests might be made to your site, such as 
+// denial of service attacks, brute force attacks, or even just a client or script that is not 
+// behaving as expected. Aside from performance issues that can arise from too many requests causing 
+// your server to slow down, you may also be charged for the additional traffic. This package can be 
+// used to limit the number of requests that can be made to a particular route or set of routes. 
 const limiter = RateLimit({
   windowMs: 1 * 60 * 1000, // 1 minute
   max: 20,
@@ -28,6 +34,13 @@ app.use(limiter);
 
 // Add helmet to the middleware chain.
 // Set CSP headers to allow our Bootstrap and Jquery to be served
+// Helmet is a middleware package. It can set appropriate HTTP headers that help protect your app from 
+// well-known web vulnerabilities
+// We normally might have just inserted app.use(helmet()); to add the subset of the security-related 
+// headers that make sense for most sites. However in the LocalLibrary base template we include some 
+// bootstrap and jQuery scripts. These violate the helmet's default Content Security Policy (CSP), 
+// which does not allow loading of cross-site scripts. To allow these scripts to be loaded we modify 
+// the helmet configuration so that it sets CSP directives to allow script loading from the indicated domains.
 app.use(
   helmet.contentSecurityPolicy({
     directives: {
@@ -35,7 +48,11 @@ app.use(
     },
   }),
 );
-
+// Web servers can often compress the HTTP response sent back to a client, significantly reducing the time 
+// required for the client to get and load the page. The compression method used will depend on the 
+// decompression methods the client says it supports in the request (the response will be sent uncompressed 
+// if no compression methods are supported).Add the compression library to the middleware chain with the 
+// use() method (this should appear before any routes you want compressed — in this case, all of them!)
 app.use(compression()); // Compress all routes
 
 mongoose.set("strictQuery", false);
